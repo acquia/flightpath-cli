e64783130b3ae005386aaf92d3c096728756a7b9
Josh Waihi <josh.waihi@acquia.com>
2022-10-28 06:52:42 +1300

    Added status check for SimpleXML extension.

--- src/EventSubscriber/Drutiny.php
+++ src/EventSubscriber/Drutiny.php
@@ -2,50 +2,63 @@
 
 namespace Drutiny\Acquia\AMA\EventSubscriber;
 
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\Console\Event\ConsoleCommandEvent;
+use Composer\Semver\Comparator;
 use Drutiny\Acquia\Plugin\AcquiaTelemetry;
+use Drutiny\Entity\RuntimeDependency;
+use Drutiny\Event\RuntimeDependencyCheckEvent;
 use Drutiny\Plugin\PluginRequiredException;
+use Symfony\Component\Console\Event\ConsoleCommandEvent;
 use Symfony\Component\Console\Exception\RuntimeException;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
-class Drutiny implements EventSubscriberInterface {
+class Drutiny implements EventSubscriberInterface
+{
+    protected AcquiaTelemetry $plugin;
+    protected $consent = false;
 
-  protected AcquiaTelemetry $plugin;
-  protected $consent = false;
+    public function __construct(AcquiaTelemetry $plugin)
+    {
+        $this->plugin = $plugin;
+    }
 
-  public function __construct(AcquiaTelemetry $plugin)
-  {
-    $this->plugin = $plugin;
-  }
+    public static function getSubscribedEvents()
+    {
+        return [
+          'console.command' => 'enforceTelemetry',
+          'status' => 'checkStatus',
+        ];
+    }
 
-  public static function getSubscribedEvents()
-  {
-    return [
-      'console.command' => 'enforceTelemetry',
-    ];
-  }
+    /**
+     * Enforce telemetry.
+     */
+    public function enforceTelemetry(ConsoleCommandEvent $event)
+    {
+        if (!file_exists(DRUTINY_LIB.'/flightpath')) {
+            return;
+        }
+        if (!in_array($event->getCommand()->getName(), ['profile:run', 'policy:audit'])) {
+            return;
+        }
+        try {
+            $config = $this->plugin->load();
+        } catch (PluginRequiredException $e) {
+            $this->plugin->setup();
+            $config = $this->plugin->load();
+        }
+        $this->consent = $config['consent'];
+        if (!$this->consent) {
+            throw new RuntimeException("Consent to send telemetry is required: Please run `./flightpath plugin:setup acquia:telemetry`.\nAll data is anonymous.");
+        }
+    }
 
-  /**
-   * Enforce telemetry.
-   */
-  public function enforceTelemetry(ConsoleCommandEvent $event)
-  {
-     if (!file_exists(DRUTINY_LIB.'/flightpath')) {
-       return;
-     }
-     if (!in_array($event->getCommand()->getName(), ['profile:run', 'policy:audit'])) {
-       return;
-     }
-     try {
-       $config = $this->plugin->load();
-     }
-     catch (PluginRequiredException $e) {
-       $this->plugin->setup();
-       $config = $this->plugin->load();
-     }
-     $this->consent = $config['consent'];
-     if (!$this->consent) {
-       throw new RuntimeException("Consent to send telemetry is required: Please run `./flightpath plugin:setup acquia:telemetry`.\nAll data is anonymous.");
-     }
-  }
+    public function checkStatus(RuntimeDependencyCheckEvent $event)
+    {
+        $event->addDependency(
+            (new RuntimeDependency('Simple XML'))
+            ->setValue(extension_loaded('simplexml'))
+            ->setDetails('Drutiny requires the SimpleXML extension')
+            ->setStatus(extension_loaded('simplexml'))
+        );
+    }
 }
